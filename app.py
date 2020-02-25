98d327676cca139ce020db7b15c1de2024c8dd4c
Rowan <rowan.copeland@gmail.com>
2020-02-25 15:09:47 +0000

    added done and edit buttons and styling. Also added submit button

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:BonjourMong0@rowanscluster-qi6ic.mongodb.net/task_manager?retryWrites=true&w=majority')
 
 mongo = PyMongo(app)
 
@@ -13,12 +13,21 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html",
+    tasks=mongo.db.tasks.find())
 
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html')
+    return render_template('addtask.html',
+    categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
